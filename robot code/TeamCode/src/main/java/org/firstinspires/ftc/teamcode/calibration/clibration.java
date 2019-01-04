d2184f99c2a0cd9bc4e6db7244ebfd4f10761c11
Black Tigers (Dell) <ronzohar15@gmail.com>
2019-01-04 16:42:48 +0200

    Starting calibration

---
+++ robot code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calibration/clibration.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.calibration;
+
+import android.hardware.Camera;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.security.Policy;
+
+public class clibration {
+    public OpMode opMode;
+    private Camera mCamera;
+    private String WhiteParameter = Camera.Parameters.WHITE_BALANCE_AUTO;
+}
